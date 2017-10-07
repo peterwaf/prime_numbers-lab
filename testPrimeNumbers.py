f7105e8dc75113de7c24eebd4218c96cdb848995
Denis Juma <kwanusud@gmail.com>
2017-10-07 20:01:22 +0300

    [bug] fix test

--- testPrimeNumbers.py
+++ testPrimeNumbers.py
@@ -7,14 +7,14 @@ class TestPrimeNumbers(unittest.TestCase):
     """ Tests prime_numbers functionality """
 
     def test_input_is_positive(self):
-        self.assertEqual(prime_numbers(-5), [])
+        self.assertEqual(primeNumbers(-5), [])
     def test_input_is_Not_a_list(self):
-    	self.assertRaises(TypeError,prime_numbers, [2,4])
+    	self.assertRaises(TypeError,primeNumbers, [2,4])
     def test_input_is_zero(self):
-        self.assertEqual(prime_numbers(0), [])
+        self.assertEqual(primeNumbers(0), [])
     def test_output_is_primeNumbers(self):
-        self.assertEqual(prime_numbers(8), [1,2,3,5,7])
+        self.assertEqual(primeNumbers(8), [1,2,3,5,7])
     def test_input_is_not_a_dictionary(self):
-        self.assertRaises(TypeError,prime_numbers, {"jane":2, "tom":4})
+        self.assertRaises(TypeError,primeNumbers, {"jane":2, "tom":4})
 if __name__ == "__main__":
     unittest.main(exit = False)
