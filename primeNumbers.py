3b9a07d13428116a3ca6c5705c8a724bad70980e
peterwaf <peterwafulah@gmail.com>
2017-10-20 17:16:57 +0300

    prime num from local files,comit

--- primeNumbers.py
+++ primeNumbers.py
@@ -1,1 +1,17 @@
-
+def primeNumbers(number):
+    the_list = []
+    
+    for i in range(2, number+1):
+      prime = True
+    
+      for x in range(2,i):
+        if i % x == 0:
+          prime = False
+        
+      if prime == True:
+            
+        the_list.append(i)
+        
+    return the_list
+    
+print(primeNumbers(20))
